ed1205ca254f79e4f12cbab59b688a0ed2637557
princeton <decoyzpr@gmail.com>
2019-07-12 13:56:45 -0400

    Got tty-prompt working. Yay! ^_^

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'tty-prompt'
 
 def clear_screen
     if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
@@ -8,3 +9,20 @@ def clear_screen
     end
 end
 
+def game_initialize
+    prompt = TTY::Prompt.new
+    clear_screen()
+    puts "Welcome to Tic-Tac-Toe where you need to get 3 in a row!"
+    puts
+    puts "Would you like to be X or O ? "
+    player_symbol = prompt.select("Choose which symbol you want to be:", %w(X O Quit))
+    if player_symbol == 'Quit'
+        clear_screen()
+        exit
+    end
+end
+
+
+
+
+game_initialize()
