a8ecd3d84602289495f4b0876114f52158d6bf0b
princeton <decoyzpr@gmail.com>
2019-07-12 17:19:17 -0400

    Changed player and computer to be global variables.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,5 +1,7 @@
 require 'pry'
 require 'tty-prompt'
+$player = ''
+$computer = ''
 
 def clear_screen
     if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
@@ -9,20 +11,33 @@ def clear_screen
     end
 end
 
-def game_initialize
-    prompt = TTY::Prompt.new
+def welcome_screen
     clear_screen()
     puts "Welcome to Tic-Tac-Toe where you need to get 3 in a row!"
     puts
+    game_initialize()
+end
+
+def game_initialize
+    prompt = TTY::Prompt.new
     puts "Would you like to be X or O ? "
-    player_symbol = prompt.select("Choose which symbol you want to be:", %w(X O Quit))
-    if player_symbol == 'Quit'
+    $player = prompt.select("Choose which symbol you want to be:", %w(X O Quit))
+    if $player == 'Quit'
         clear_screen()
         exit
     end
+    if $player == 'X'
+        $computer = 'O'
+    else
+        $computer = 'X'
+    end
+    start_game()
+end
+
+def start_game
+    puts $player
+    puts $computer
 end
 
 
-
-
-game_initialize()
+welcome_screen()
